7e4f2746b7912653114b778c7eb94dd13c48a142
Jamshidbek97 <urologiya@internet.ru>
2025-07-13 11:36:01 +0900

    Feat: Developing app.tsx, creating routes and declare types

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+
 import "../css/app.css";
 
 function App() {
-  return <div>HOME</div>;
+  const location = useLocation();
+  return (
+    <>
+      {location.pathname === "/" ? "HomeNavbar" : "OtherNavbar"}
+      <Switch>
+        <Route path="/products">ProductsPage</Route>
+        <Route path="/orders">OrderPage</Route>
+        <Route path="/member-page">UserPage</Route>
+        <Route path="/help">HelpPage</Route>
+        <Route path="/">HomePage</Route>
+      </Switch>
+      Footer
+    </>
+  );
 }
 
 export default App;
-
-
-
-
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import {createRoot} from "react-dom/client"
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import CssBaseline from "@mui/material/CssBaseline"
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import {BrowserRouter as Router} from "react-router-dom"
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
+import ContextProvider from "./app/context/ContextProvider";
 import "./css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ContextProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Router>
+            <App />
+          </Router>
+        </CssBaseline>
+      </ThemeProvider>
+      </ContextProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
