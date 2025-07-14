e8058def33fe79e3a71d2017a504923bb449b2b8
Jamshidbek97 <urologiya@internet.ru>
2025-07-14 18:08:57 +0900

    Feat: Publish popular books screen component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Statistics from "./Statistics";
 import "../../../css/home.css";
 import PopularBooks from "./PopularBooks";
+import NewBooks from "./NewBooks";
 
 export default function HomePage() {
   return (
     <div className="homepage">
       <Statistics />
       <PopularBooks />
+      <NewBooks />
     </div>
   );
 }
