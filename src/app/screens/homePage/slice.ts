f87dbc35ce72865cd686fedc9f92ef75f34af3da
Jamshidbek97 <urologiya@internet.ru>
2025-07-19 06:42:05 +0900

    Feat: Create slice for HomePage component

---
+++ src/app/screens/homePage/slice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { HomePageState } from "../../../lib/types/screen";
+
+const initialState: HomePageState = {
+  popularBooks: [],
+  newBooks: [],
+  spotlightMembers: [],
+};
+
+const homePageSlice = createSlice({
+  name: "homePage",
+  initialState,
+  reducers: {
+    setPopularBooks: (state, action) => {
+      state.popularBooks = action.payload;
+    },
+    setNewBooks: (state, action) => {
+      state.newBooks = action.payload;
+    },
+    setSpotlightMembers: (state, action) => {
+      state.spotlightMembers = action.payload;
+    },
+  },
+});
+
+export const { setNewBooks, setPopularBooks, setSpotlightMembers } =
+  homePageSlice.actions;
+
+const HomePageReducer = homePageSlice.reducer;
+export default HomePageReducer;
